db8a4680a733d7bf1c747dcfcb0b6fc94f9b66a7
RickardGS4991 <rickard928@gmail.com>
2026-02-12 00:21:58 -0600

    feature/tandapi-1020 New changes from dependencies

---
+++ src/main/resources/db/migration/V1__init.sql
@@ -0,0 +1,18 @@
+CREATE TABLE users (
+                       user_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+
+                       first_name   TEXT NOT NULL,
+                       last_name    TEXT NOT NULL,
+                       birthdate    DATE NOT NULL,
+
+                       username     TEXT NOT NULL UNIQUE,
+                       email        TEXT NOT NULL UNIQUE,
+                       password     TEXT NOT NULL,
+
+                       address      TEXT NOT NULL,
+                       postal_code  TEXT NOT NULL,
+                       city         TEXT NOT NULL,
+
+                       created_at   TIMESTAMPTZ NOT NULL DEFAULT now(),
+                       updated_at   TIMESTAMPTZ NOT NULL DEFAULT now()
+);
