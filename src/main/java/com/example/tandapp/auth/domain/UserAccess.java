a102272167176c90ae3952fd2e7b6ea0cfa6bd1a
RickardGS4991 <rickard928@gmail.com>
2026-02-18 00:02:42 -0600

    feature/tandapi-1040 New columns added

--- src/main/java/com/example/tandapp/auth/domain/UserAccess.java
+++ src/main/java/com/example/tandapp/auth/domain/UserAccess.java
@@ -1,26 +1,32 @@
 package com.example.tandapp.auth.domain;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
 
 @Embeddable
 public class UserAccess {
 
+    @Column(nullable = false)
     private String username;
+
+    @Column(nullable = false)
     private String email;
-    private String passwordHash;
+
+    @Column(nullable = false)
+    private String password;
 
     protected UserAccess(){}
 
-    public UserAccess(String username, String email, String passwordHash){
+    public UserAccess(String username, String email, String password){
         if(username == null || username.isBlank()) throw new IllegalArgumentException("Username inválido.");
         if(email == null || email.isBlank()) throw new IllegalArgumentException("Correo electrónico inválido.");
-        if(passwordHash == null || passwordHash.isBlank()) throw new IllegalArgumentException("Contraseña inválida.");
+        if(password == null || password.isBlank()) throw new IllegalArgumentException("Contraseña inválida.");
 
         validateEmail(email);
 
         this.username = username;
         this.email = email;
-        this.passwordHash = passwordHash;
+        this.password = password;
     }
 
     public String getUsername(){
@@ -31,8 +37,8 @@ public class UserAccess {
         return email;
     }
 
-    public String getPasswordHash(){
-        return passwordHash;
+    public String getPassword(){
+        return password;
     }
 
     private void validateEmail(String email){
