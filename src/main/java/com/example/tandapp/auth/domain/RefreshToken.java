d32fcfc9910870e178706fb93d55bd84236d7c64
RickardGS4991 <rickard928@gmail.com>
2026-02-19 00:31:28 -0600

    feature/tandapi-1050 New entity: refreshToken

---
+++ src/main/java/com/example/tandapp/auth/domain/RefreshToken.java
@@ -0,0 +1,55 @@
+package com.example.tandapp.auth.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+import java.time.Instant;
+
+import java.util.UUID;
+
+@Entity
+public class RefreshToken {
+    @Id
+    private UUID refreshTokenId;
+
+    @Column(nullable = false)
+    private UUID userId;
+
+    @Column(nullable = false)
+    private String refreshToken;
+
+    @Column(nullable = false)
+    private Instant expiresAt;
+
+    @Column(nullable = false)
+    private boolean revoked;
+
+    protected RefreshToken(){}
+
+    public RefreshToken(UUID userId, String refreshToken, Instant expiresAt, boolean revoked){
+        if(userId == null) throw new IllegalArgumentException("UserID inválido. Usuario no tiene permisos.");
+        if(refreshToken == null || refreshToken.isBlank()) throw new IllegalArgumentException("Token inválido.");
+        if(expiresAt == null) throw new IllegalArgumentException("Fecha inválida.");
+
+        this.userId = userId;
+        this.refreshToken = refreshToken;
+        this.expiresAt = expiresAt;
+        this.revoked = revoked;
+    }
+
+    public boolean isExpired(Instant todayHour){
+        Instant thisMoment = Instant.now();
+
+        return thisMoment.isAfter(todayHour);
+    }
+
+    public boolean isRevoked(){
+        return revoked;
+    }
+
+    public String showRToken(){
+        return refreshToken;
+    }
+}
+
